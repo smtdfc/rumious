6f5e008de33a515096a1dce14e6966f276f6e38d
smtdfc <smtdfc.master@gmail.com>
2025-03-02 15:24:34 +0700

    feat(ui) : Add accordion component

---
+++ packages/ui/src/js/components/accordion.js
@@ -0,0 +1,25 @@
+export class RumiousUIAccordion {
+  constructor(element) {
+    this.element = element;
+  }
+  
+  static generator(element) {
+    return new RumiousUIAccordion(element);
+  }
+  
+  open() {
+    this.element.classList.add("open");
+  }
+  
+  close() {
+    this.element.classList.remove("open");
+  }
+  
+  toggle() {
+    if (this.element.classList.contains("open")) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+}
--- packages/ui/src/js/components/index.js
+++ packages/ui/src/js/components/index.js
@@ -2,3 +2,4 @@ export * from "./navbar.js";
 export * from "./offcanvas.js";
 export * from "./overlay.js";
 export * from "./sidebar.js";
+export * from "./accordion.js";
