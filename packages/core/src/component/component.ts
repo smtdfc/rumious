d2e2cdcce9782c6807dd7a2e0585fefbcfe392a7
smtdfc <smtdfc.master@gmail.com>
2025-07-20 16:44:37 +0700

    fix(core): Fix override property in Component class

--- packages/core/src/component/component.ts
+++ packages/core/src/component/component.ts
@@ -6,7 +6,6 @@ export class Component<T extends object> {
   static tagName = 'rumious-component';
   private renderContext: RenderContext;
   public children: RenderContent | null = null;
-  public element: HTMLElement | null = null;
   
   constructor(
     protected props: T,
