aa94faf3bed05098c5e91ac5de716038a08af9ce
smtdfc <smtdfc.master@gmail.com>
2025-08-01 21:38:37 +0700

    test(core): Add component testcase

--- packages/core/src/app/app.ts
+++ packages/core/src/app/app.ts
@@ -50,7 +50,7 @@ export class App {
       }
     }
   }
-
+  
   setRootLayout(layout: any) {
     this.rootLayout = layout;
   }
--- packages/core/test/index.js
+++ packages/core/test/index.js
@@ -4,4 +4,5 @@ import initEnv from 'testious-browser-env';
 initEnv();
 
 await import("./app.js");
+await import("./component.js");
 runTests();
