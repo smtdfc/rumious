1eb4bf7cf8f9503d16144236cf049e3850d3f3fc
smtdfc <smtdfc.master@gmail.com>
2025-10-08 00:35:53 +0700

    chore: Update version

--- packages/rollup-plugin/rollup.config.js
+++ packages/rollup-plugin/rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import os from 'os';
