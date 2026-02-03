d1bc792f54608949d9f39b83eccd7cc4a3c86dd1
smtdfc <smtdfc.master@gmail.com>
2026-02-03 23:39:06 +0700

    feat(vite-plugin): add initial plugin structure with TypeScript support

---
+++ packages/vite-plugin/src/index.ts
@@ -0,0 +1,26 @@
+import { Compiler } from "@rumious/compiler";
+import type { Plugin } from "vite";
+
+export default function myTsPlugin(): Plugin {
+  const compiler = new Compiler();
+  return {
+    name: "vite-plugin-rumious",
+
+    apply: "serve",
+    config(config) {},
+
+    transform(code: string, id: string) {
+      if (id.endsWith(".tsx") || id.endsWith(".jsx")) {
+        const result = compiler.compile(code, {
+          filename: id,
+          sourceMapFile: id,
+          strictMode: true,
+        });
+        return {
+          code: result.code,
+          map: result.map,
+        };
+      }
+    },
+  };
+}
